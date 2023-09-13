ccb52d327722aec17485723ec1f211ab1987bf56
akosilar <55524614+akosilar@users.noreply.github.com>
2023-09-13 19:13:12 -0400

    created views for about and index

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,10 +5,14 @@
         }
 
         public function index() {
-
+            $data = [
+                'title' => 'welcome'
+            ];
+            $this->view('pages/index', $data);
         }
 
         public function about() {
+            $this->view('pages/about');
 
     }
 
