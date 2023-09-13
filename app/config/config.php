addaa0c4748d9c4ed0ff3bbffca427771bdafa21
akosilar <55524614+akosilar@users.noreply.github.com>
2023-09-13 19:28:38 -0400

    created config.php and autoloaded libraries in bootstrap.php

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+    // app root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // url root
+    define('URLROOT', 'http://localhost/larsmvc/');
+    // site name
+    define('SITENAME', 'LarsMVC');
