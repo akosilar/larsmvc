477ac947d11ee564871c88d2a2d80a274c870ad5
Lars Dev <janlarsaustero@gmail.com>
2023-09-14 00:19:49 -0400

    started creating database

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,10 @@
 <?php
+    // db params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '123456');
+    const DB_NAME = 'tmvc';
+
     // app root
     define('APPROOT', dirname(dirname(__FILE__)));
     // url root
